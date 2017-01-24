e4a475c2bc9c3f2f54db16059265cabe11270146
joncorrin <jcorrin@udel.edu>
2017-01-24 15:06:04 -0500

    Added imageuploader

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -69,6 +69,6 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :city, :state, :website)
+      params.require(:place).permit(:name, :city, :state, :website, :image)
     end
 end

8aac5dfef7bde830e256350647d0507facc199cc
joncorrin <jcorrin@udel.edu>
2017-01-24 19:29:22 -0500

    Added mini_magick gem

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'carrierwave'
 gem 'fog-aws', '1.2.0'
 gem 'figaro'
 gem 'devise'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+ include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
 
-  if Rails.env.production?
-    store :fog
-  else
-    store :file
-  end
+    if Rails.env.production?
+      storage :fog
+    else
+      storage :file
+    end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,7 +35,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+     process resize_to_fit: [200, 300]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.

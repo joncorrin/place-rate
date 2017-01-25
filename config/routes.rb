766ab27f44070cf26592ac8af05c38983467c6f9
joncorrin <jcorrin@udel.edu>
2017-01-25 16:16:26 -0500

    Added links for registration

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
   get  '/show',    to: 'places#show'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

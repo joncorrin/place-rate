e014fa1c7b1ff924cef74a009a2926ac3d81ec06
joncorrin <jcorrin@udel.edu>
2017-01-27 12:16:35 -0500

    Added validations to forms

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,15 @@ class Place < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :reviews
+
+  validates :name, :city, :state, presence: true
+
+  validates :state, format: {with: /\A[A-Z]{2}\z/,
+    message: "must be in format: PA"}
+
+  validates :website, format: {with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://"}
+
+  validates :city, format: {with: /\A[a-zA-z]\z/,
+    message: "city can only contain letters"}
 end
